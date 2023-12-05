3e710a76f772c2f5b6ca71c3f60dfdab2127d7d7
Hiva7 <118381485+Hiva7@users.noreply.github.com>
2023-12-05 10:04:49 +0700

    hi

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 
 <template>
   <div>
-    <router-link to="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs">Hello World!</router-link>
+    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs">Hello World!</a>
   </div>
 </template>
 
