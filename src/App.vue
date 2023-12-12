c4d716d31a9ebcf35da49f0a6daff0d96e3c5c34
Suchea <bsuchea@gmail.com>
2023-12-12 07:30:23 +0700

    apply checkbox binding with binding vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,37 @@
 
 <template>
-  <div>
-    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs">Hello World!</a>
-  </div>
+  <section>
+    <h2>Assigments Undone ({{ assigments.filter(a => !a.completed).length }})</h2>
+    <ul>
+      <li v-for="ret in assigments.filter(a => !a.completed)" :key="ret.id"> 
+        {{ ret.name }} 
+        <input type="checkbox" v-model="ret.completed">
+      </li>
+    </ul>
+  </section>
+  <br>
+  <section>
+    <h2>Assigments done ({{ assigments.filter(a => a.completed).length }})</h2>
+    <ul>
+      <li v-for="ret in assigments.filter(a => a.completed)" :key="ret.id"> 
+        {{ ret.name }} 
+        <input type="checkbox" v-model="ret.completed">
+      </li>
+    </ul>
+  </section>
 </template>
+<script>
+export default{
+  data() {
+    return {
+      assigments: [
+        {id: 1, name: 'Web application', completed: false},
+        {id: 2, name: 'Desktop with c#', completed: false},
+        {id: 3, name: 'Marketing project', completed: false},
+        {id: 4, name: 'Vue Projects', completed: false},
+      ]
+    }
+  }
+}
 
+</script>
